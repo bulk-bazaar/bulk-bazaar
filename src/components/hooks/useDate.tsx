182fba421fa8b47f763ac75ebdc912120cafece0
gajendrakumartwinwal <gajendrakumartwinwal@gmail.com>
2024-09-17 19:00:10 +0530

    fix: aded

--- src/components/hooks/useDate.tsx
+++ src/components/hooks/useDate.tsx
@@ -4,6 +4,7 @@ const useDate = (date: string): string => {
   const month: number = fullDate.getMonth() + 1;
   const day: number = fullDate.getDate();
 
+
   const dateFormated: string =
     month.toString().padStart(2, "0") +
     "/" +
